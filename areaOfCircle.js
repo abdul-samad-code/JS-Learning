2a0ad109d802d47ddac3c87f0d04e668b3dcf17d
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-15 22:16:44 +0530

    Program for calculating area of circle

---
+++ areaOfCircle.js
@@ -0,0 +1,8 @@
+// A program to calculate the area of circle
+
+// formula to calculate the area of a circle "area = (lenght * breadth)"
+
+let lenght = 7;
+let breadth = 7;
+// let area = (lenght * breadth);
+console.log(lenght*breadth);
