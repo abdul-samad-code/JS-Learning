7100a2ae2ebb3add411170b2931bfede94ffd762
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-17 23:49:27 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,14 @@
+// this code refers to coversion of string to number and number to string with add some more values 
+
+let myStr = "25"
+let number = Number(myStr)+5;
+console.log(number);
+console.log(typeof number);
+
+
+
+let num = 10;
+let str = String(num)+"rupee";
+console.log(str);
+console.log(typeof str);
+ 
