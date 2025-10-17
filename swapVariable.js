cdeeadab2d59b30b148307be357ba15169c4392f
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-17 23:49:06 +0530

    Add files via upload

---
+++ swapVariable.js
@@ -0,0 +1,20 @@
+// swapping two variable without using third variable
+
+
+let num1 = 3;
+let num2 = 7;
+// let temp = num1;
+// num1 = num2;
+// num2 = temp;
+// console.log(num1, num2);
+
+// num1 = num1+num2; //num1 = 10
+// num2 = num1-num2; //num2 = 3
+// num1 = num1-num2; //num1 = 7
+// console.log(num1, num2);
+
+[num1, num2] = [num2, num1]
+
+console.log(num1, num2);
+
+
