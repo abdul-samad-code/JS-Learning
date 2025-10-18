cc7c0a764edb4ffe4f8617efdeee18762e5ca3ef
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-18 22:39:40 +0530

    added task 2 or 3

---
+++ Strings/task_2.js
@@ -0,0 +1,12 @@
+// Task 2: Extract part of a string   //  Use .slice() to extract a specific part of a string.
+
+let name = "Abdul Samad"
+console.log(name.slice(0, 5));
+
+
+
+
+// let str = "Abdul Samad";
+// let part = str.slice(0, 5);
+// console.log(part);
+
---
+++ Strings/task_3.js
@@ -0,0 +1,15 @@
+//   Task 3 : Replace text and trim spaces
+
+
+let sentence = "       I love coding in Java       "
+console.log(sentence.trim());
+console.log(sentence.replace("Java", "JavaScript"));
+
+
+
+// let trim = sentence.trim();
+// let replace = sentence.replace("Java", "JavaScript");
+// console.log(trim);
+// console.log(replace);
+
+
