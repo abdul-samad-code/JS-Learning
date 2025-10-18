6ac18fdef08c7e8005f72338256d09c8c5886d60
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-18 22:38:24 +0530

    added task 1

---
+++ Strings/task_1.js
@@ -0,0 +1,15 @@
+//Task 2: print, Change Case and print length of string
+
+
+let str = "Abdul Samad"
+// let length = str.length;
+// let upperCase = str.toUpperCase();
+// let lowerCase = str.toLowerCase();
+// console.log(str, length, upperCase, lowerCase);
+
+console.log(str);
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+
