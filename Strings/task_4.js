9a64fc4dd96ee29ca8bcb1332cafe3df6be953cf
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-19 22:40:19 +0530

    Added two task

---
+++ Strings/task_4.js
@@ -0,0 +1,6 @@
+//     Task 4: Split a string      //  Use .split() to break a sentence into words.
+
+
+let sentence = "I am learning currently JavaScript"
+let word = sentence.split(" ");
+console.log(word);
---
+++ Strings/task_5.js
@@ -0,0 +1,9 @@
+///    Task 5: Join strings    //  Use .concat() or + to join multiple strings.
+
+let name = "Abdul Samad"
+// let surname = " Shiekh"
+// let concat = name.concat(surname)
+// console.log(concat);
+
+
+console.log(name.concat(" Sheikh"));
