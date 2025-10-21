0605559df0014c6b5a4ed09a459e59509295cebc
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 18:23:55 +0530

    another 4th one is completed

---
+++ Math/task_04.js
@@ -0,0 +1,11 @@
+//  Task 4 — Random Numbers
+//  Print:
+// A random number between 0 and 1
+// A random number between 1 and 10
+// A random number between 1 and 100
+//  Hint: Use
+// Math.floor(Math.random() * max) + 1
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*100)+1);
