496b487d0f407639b39f1fabb7f0c49320fc72aa
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 18:19:54 +0530

    task 5 done

---
+++ Numbers/task_05.js
@@ -0,0 +1,22 @@
+// Task 5: isNaN() and isFinite()
+// let n1 = 25;
+// let n2 = "hello";
+// let n3 = Infinity;
+
+
+// Check:
+
+// Which value is NaN
+
+// Which value is finite
+
+let n1 = 25;
+let n2 = "hello";
+let n3 = Infinity;
+
+console.log(isNaN(n1));
+console.log(isNaN(n2));
+console.log(isNaN(n3));
+console.log(isFinite(n1));
+console.log(isFinite(n2));
+console.log(isFinite(n3));
