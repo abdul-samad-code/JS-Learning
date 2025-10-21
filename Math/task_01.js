b9903837f4aabc62d395147a64eb5c22a287f80b
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 18:22:16 +0530

    first task completed

---
+++ Math/task_01.js
@@ -0,0 +1,21 @@
+
+// Task 1 — Rounding Methods
+// let num = 7.56;
+//  Print:
+// Rounded value using Math.round()
+// Rounded up using Math.ceil()
+// Rounded down using Math.floor()
+// Truncated value using Math.trunc()
+
+
+
+
+let num = 7.56;
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.trunc(num));
+
+
+
+
