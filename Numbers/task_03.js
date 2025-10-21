9109d6d0ca0b5494df30096892d82eaf3ca9037a
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 18:18:06 +0530

    completed

---
+++ Numbers/task_03.js
@@ -0,0 +1,18 @@
+
+
+
+// Task 3: Number() Conversion
+// let a = "50.25";
+// let b = "25 apples";
+
+
+// Convert both into numbers and print the result.
+// Observe the output — which one gives NaN?
+
+
+let a = "50.25";
+let b = "25 apples";
+console.log( Number(a));
+console.log(typeof a);
+console.log(Number(b));
+console.log(typeof b);
