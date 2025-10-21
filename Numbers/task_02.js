b396158c2894f049762ec78519913242c9094e6a
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 15:50:42 +0530

    task 2 done

---
+++ Numbers/task_02.js
@@ -0,0 +1,13 @@
+//  Task 2: toPrecision()
+// let num = 123.4567;
+
+
+//  Print:
+
+// The number with 3 digits precision
+
+// The number with 5 digits precision
+
+let num = 123.4567;
+console.log(num.toPrecision(3))
+console.log(num.toPrecision(5))
