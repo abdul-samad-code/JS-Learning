d255bcb2778e300f0ecc7dc7e6397d62479912bc
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 18:18:57 +0530

    completed 4th task

---
+++ Numbers/task_04.js
@@ -0,0 +1,25 @@
+
+// Task 4: parseInt() and parseFloat()
+// let value1 = "40.78";
+// let value2 = "99px";
+
+
+// 👉 Print:
+
+// parseInt(value1)
+
+// parseFloat(value1)
+
+// parseInt(value2)
+
+// parseFloat(value2)
+
+// 💡 Note: Compare which keeps the decimal and which doesn’t.
+
+let value1 = "22.4545";
+let value2 = "99px65";
+
+console.log(parseInt(value1));
+console.log(parseFloat(value1));
+console.log(parseInt(value2));
+console.log(parseFloat(value2));
