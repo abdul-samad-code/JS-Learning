570e170e0dbff847e02a0c5485f92faa33684190
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-21 15:50:03 +0530

    Task 1 completed

---
+++ Numbers/task_01.js
@@ -0,0 +1,15 @@
+// 🟢 Task 1: toFixed()
+// let price = 199.98765;
+
+
+// 👉 Print:
+
+// The price with 2 decimal places
+
+// The price with 0 decimal places
+
+
+let price = 199.98765;
+
+console.log(price.toFixed(2));
+console.log(price.toFixed(0))
