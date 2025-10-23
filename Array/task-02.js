53ba10a465025babd0927f77fae7e3da82ae4d23
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-23 20:14:04 +0530

    task 2 done

---
+++ Array/task-02.js
@@ -0,0 +1,11 @@
+
+// Task 2: Array Length and Update
+
+
+
+let numbers = [43, 32, 54, 12, 32];
+console.log(" the Length of array : ",numbers.length);
+numbers[2] = 99;                                                 
+console.log(numbers);
+
+
