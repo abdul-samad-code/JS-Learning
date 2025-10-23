67dd5a6726a5bf823a62866157503a252c1cbc51
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-23 20:13:18 +0530

    Add files via upload

---
+++ Array/task_01.js
@@ -0,0 +1,11 @@
+
+// Task 1: Create and Access Elements
+
+// Goal: Practice creating arrays and accessing values.
+
+//  1. Create an array called 'fruits' with 5 fruit names.
+//  2. Print the first and last fruit name.
+
+let fruits = ["mango", "apple", "banana", "grapes", "watermelon"];
+console.log("the first fruit : ",fruits[0]);                              // 0 first index of array , print first elements
+console.log("The last fruit : ",fruits[fruits.length-1]);                 // this will print last element   
