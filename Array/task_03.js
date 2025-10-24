d69d688411951ee428f1d3909024322cf0591374
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-24 20:48:35 +0530

    Add files via upload

---
+++ Array/task_03.js
@@ -0,0 +1,26 @@
+
+// Task 3: Add and Remove Elements
+
+// Goal: Use push(), pop(), shift(), and unshift().
+
+// 1. Create an array of 3 colors.
+// 2. Add one color at the end.
+// 3. Add one color at the beginning.
+// 4. Remove the last color.
+// 5. Print the final array.
+
+let colors = ["Green", "Blue", "Yellow",];
+console.log(colors);
+colors.push("Red");
+console.log(colors);
+colors.unshift("Vilot");
+console.log(colors);
+colors.pop();
+console.log(colors);
+
+
+
+
+
+
+
