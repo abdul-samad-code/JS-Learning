2ce326192982537a388018c7a0e253805663ff07
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-24 20:49:17 +0530

    Add files via upload

---
+++ Array/task_04.js
@@ -0,0 +1,24 @@
+
+// Task 4: Join and Split
+
+// Goal: Convert arrays to strings and vice versa.
+
+// 1. Create an array: ["I", "love", "JavaScript"]
+// 2. Join it into a single string with spaces.
+// 3. Then split the string back into an array.
+
+let arr = ["I", "love", "JavaScript"]
+console.log(arr);
+let newArr = arr.join(" ")
+console.log(newArr);
+console.log(typeof newArr);
+let backArr = newArr.split(" ");
+console.log(backArr);
+console.log(typeof backArr);
+
+
+
+
+
+
+
