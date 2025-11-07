cc195564c0d48f2501992f377ff0b4318e47d3fc
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-07 23:07:54 +0530

    Add files via upload

---
+++ Control Flow/one.js
@@ -0,0 +1,14 @@
+// *****************  Control Floww stetement  ***********************************8
+//  if statement
+
+
+let age = 13;
+if (age >= 18){
+    console.log("You are eligible for voting");
+} else {
+    console.log("You are not eligible for vote");
+    
+}
+
+
+
