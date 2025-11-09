d186847db868c10f36741aa4f161ffbc8d29b272
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-09 23:02:28 +0530

    calculator

---
+++ Switch case/twoNumberCalc.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+let a = Number(prompt("Enter The First Number   : "));
+let b = Number(prompt("Enter the Second Number  : "));
+let op = prompt("Enter the Operator [+ - * /]   : ");
+switch (op){
+    case "+":
+        console.log("Result : ",a+b);
+        break;
+    case "-":
+        console.log("Result : ",a-b);
+        break;
+    case "*":
+        console.log("Result : ",a*b);
+        break;
+    case "/":
+        console.log("Result : ",a/b);
+        break;  
+}
