e93040fee3d0bd9084166cbe5aff478642e40051
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-13 21:37:13 +0530

    Add files via upload

---
+++ Loops/demo.js
@@ -0,0 +1,57 @@
+// for loop
+
+
+// for (let i=0; i<=10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+
+
+
+
+
+// for (let i = 0; i<=10; i++) {
+//     console.log("Outer loop");
+//     for (let j = 0; j <= 0; i++) {
+//            console.log(`Inner loop value ${i} inner loop value ${i}`);
+//           console.log(i+"+"+j+"="+i*j);
+           
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+// let myArr = ["flash", "batman",  "superman"]
+// console.log(myArr.length);
+// for (let index = 0; index <= myArr; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+    
+    
+// }
+
+
+
+
+// break & continue
+
+for (let index = 0; index <= 20; index++) {
+    if (index==5) {
+        console.log("detected 5");
+        continue
+    }
+   console.log(`value of index ia ${index}`);
+   
+    
+}
+
